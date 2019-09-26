06bb8f77a6e4042b165624a5f7277abf5679f3f5
tom <gaoqiang19514@163.com>
2019-09-26 12:26:05 +0800

    添加创建目录功能

--- index.js
+++ index.js
@@ -13,10 +13,34 @@ const DOWNLOAD_PATH = "./images";
  * 获取文件名
  * @param {String} url
  */
-function getFilename(url) {
-  const filename = url.split("/");
-  const lastIndex = filename.length - 1;
-  return path.join(DOWNLOAD_PATH, filename[lastIndex]);
+function getFilename(dir, url) {
+  if (!url) {
+    return null;
+  }
+  const temp = url.split("/");
+  const len = temp.length;
+  const filename = temp[len - 1];
+  return path.join(dir, filename);
+}
+
+/**
+ * 将字符字符串转为数字字符串
+ * @param {String} str 
+ */
+function str2Num(str) {
+  let result = 0;
+  for (let i = 0; i < str.length; i++) {
+    let code = str[i].charCodeAt();
+    result = result + code;
+  }
+  return String(result);
+}
+
+/**
+ * 获取目录
+ */
+function getDir(url) {
+  return path.join(DOWNLOAD_PATH, str2Num(url));
 }
 
 /**
@@ -62,41 +86,42 @@ function init(url, selector, attr) {
       return Promise.all(promises).then(res => {
         return {
           resArr: res,
+          imgPathArr: imgPathArr,
           totalCount: imgPathArr.length
         };
       });
     })
     .then(res => {
       let count = 0;
-      res.resArr.forEach(item => {
-        // 创建文件流
-        let img = new Readable();
-        img.push(item.data);
-        img.push(null);
-        img
-          .pipe(fs.createWriteStream(getFilename(item.config.url)))
-          .on("close", () => {
-            count = count + 1;
-            if (count === res.totalCount) {
-              console.log(`下载完成，共计：${res.totalCount}张。`);
-            }
-          });
+
+      const dir = getDir(url);
+      fsExtra.ensureDir(dir).then(() => {
+        res.resArr.forEach(item => {
+          // 创建文件流
+          let img = new Readable();
+          img.push(item.data);
+          img.push(null);
+          img
+            .pipe(fs.createWriteStream(getFilename(dir, item.config.url)))
+            .on("close", () => {
+              count = count + 1;
+              if (count === res.totalCount) {
+                console.log(`下载完成，共计：${res.totalCount}张。`);
+              }
+            });
+        });
       });
     })
     .catch(err => {
-      console.log("err", err.code);
+      console.log("err", err);
     });
 }
 
-fsExtra.emptyDir("./images", err => {
-  if (err) return console.error(err);
+console.log("程序开始");
+// init("https://www.yimg.net/sex/13178.html", "#masonry img", "data-original");
 
-  console.log("程序开始");
-  init("https://www.yimg.net/sex/13018.html", "#masonry img", "data-original");
-});
-
-// const url = "https://www.yimg.net/sex/13251.html"; 
-// 壁纸 getCurrPageImgPath($, '#masonry img', "data-original");
-
-// const url = "http://www.mlito.com/photo/girl/g_model/194916.html";
-// 美女 getCurrPageImgPath($, '.j_contl_main .alignnone', "src");
+init(
+  "http://www.mlito.com/photo/girl/g_model/194916.html",
+  ".j_contl_main .alignnone",
+  "src"
+);
