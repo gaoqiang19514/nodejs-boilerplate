2765cfb727dafa28b4dbe774a28b2cd7005bb840
tom <gaoqiang19514@163.com>
2019-09-26 14:41:29 +0800

    更新为一张一张的保存，而不是全部下载完成再保存

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const axios = require("./axios");
 const DOWNLOAD_PATH = "./images";
 
 /**
- * 获取文件名
+ * 获取文件路径
  * @param {String} url
  */
-function getFilename(dir, url) {
+function getFilePath(dir, url) {
   if (!url) {
     return null;
   }
@@ -24,8 +24,21 @@ function getFilename(dir, url) {
 }
 
 /**
+ * 获取文件名
+ * @param {String} url
+ */
+function getFilename(url) {
+  if (!url) {
+    return null;
+  }
+  const temp = url.split("/");
+  const filename = temp[temp.length - 1];
+  return filename;
+}
+
+/**
  * 将字符字符串转为数字字符串
- * @param {String} str 
+ * @param {String} str
  */
 function str2Num(str) {
   let result = 0;
@@ -62,53 +75,60 @@ function getCurrPageImgPath($, selector, attr) {
 }
 
 /**
+ * 创建文件流
+ * @param {String} data
+ */
+function createStream(data) {
+  const stream = new Readable();
+  stream.push(data);
+  stream.push(null);
+  return stream;
+}
+
+/**
  * 程序初始化
  * @param {String} url
  * @param {String} selector
  * @param {String} attr
  */
-function init(url, selector, attr) {
+function init(pageUrl, selector, attr) {
   axios
-    .get(url)
+    .get(pageUrl)
     .then(res => {
-      console.log("获取网页");
+      console.log("成功获取网页内容。");
       return cheerio.load(res.data);
     })
     .then($ => {
+      let count = 0;
+      // 获取目录
+      const dir = getDir(pageUrl);
+      //  获取上文页面中的图片地址列表
       const imgPathArr = getCurrPageImgPath($, selector, attr);
+      //  当前页面请求数量
+      const imgTotalCount = imgPathArr.length;
 
       console.log(`开始下载：`);
-      const promises = [];
-      imgPathArr.map(url => {
-        promises.push(axios.get(url));
-      });
-
-      return Promise.all(promises).then(res => {
-        return {
-          resArr: res,
-          imgPathArr: imgPathArr,
-          totalCount: imgPathArr.length
-        };
-      });
-    })
-    .then(res => {
-      let count = 0;
-
-      const dir = getDir(url);
-      fsExtra.ensureDir(dir).then(() => {
-        res.resArr.forEach(item => {
-          // 创建文件流
-          let img = new Readable();
-          img.push(item.data);
-          img.push(null);
-          img
-            .pipe(fs.createWriteStream(getFilename(dir, item.config.url)))
-            .on("close", () => {
+      imgPathArr.map(imgUrl => {
+        // 创建文件夹
+        fsExtra.ensureDir(dir).then(() => {
+          const filepath = getFilePath(dir, imgUrl);
+          const filename = getFilename(imgUrl);
+          // 对图片发起请求
+          axios.get(imgUrl).then(res => {
+            // 将字符串转换成文件流
+            const file = createStream(res.data);
+            file.pipe(fs.createWriteStream(filepath)).on("close", () => {
               count = count + 1;
-              if (count === res.totalCount) {
-                console.log(`下载完成，共计：${res.totalCount}张。`);
+              console.log(
+                `下载完成：${filename}, 进度：${parseInt(
+                  (count / imgTotalCount) * 100
+                )}%`
+              );
+              if (count === imgTotalCount) {
+                console.log(`下载完成，共计：${imgTotalCount}张。`);
               }
             });
+          });
         });
       });
     })
